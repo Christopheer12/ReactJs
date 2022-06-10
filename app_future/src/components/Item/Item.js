e3d998240270c4fa8c6ad4b3fb028090c9ec9550
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-09 23:27:24 -0300

    master

--- app_future/src/components/Item/Item.js
+++ app_future/src/components/Item/Item.js
@@ -1,21 +1,45 @@
-const task = new Promise((resolve, reject)=>{
-let condicion = false
-if(condicion) {
-    resolve('200 perfecto')
-    }else{
-        reject('404 no entrado')
-    }
+let menu =[
+{
+    id:'1',
+    nombre: 'pollo',
+    descripcion:'bbq',
+    stock: 5,
+},
 
+{
+    id:'2',
+    nombre: 'carne',
+    descripcion:'española',
+    stock: 5,
+},
+{
+    id:'3',
+    nombre: 'pescado',
+    descripcion:'santo',
+    stock: 8,
+},
 
-})
-
-const Item =()=>{
-    console.log(task)
-
-    return(
-        <div>Item</div>
-    )
+];
+const getFetch =() =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve(menu)
+        }, 3000);
+       
+    })
 }
 
+const Item =()=>{
+    getFetch()
+    .then((primero)=>{
+        console.log(primero)
+    })
+    .catch(error => console.log(error))
+    .finally(()=> console.log('biemvenido'))
+
+    return(
+        <div> Item</div>
+    )
+}
 
 export default Item
