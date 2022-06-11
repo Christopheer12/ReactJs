5ed6ab19f2e8047fa9734a271638ff19c772d792
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-11 16:53:21 -0300

    co

--- app_future/src/components/itemListContainer/ItemListContainer.js
+++ app_future/src/components/itemListContainer/ItemListContainer.js
@@ -1,15 +1,20 @@
+import {useEffect, useState} from 'react';
 import getFetch from "../../utils/getFetch/getFetch";
 
 
-const ItemListContainer =()=>{
 
-        getFetch()
+const ItemListContainer =()=>{
+    const [products,setProducts] = useState([])
+useEffect(()=>{
+       getFetch()
         .then((resp)=>{
             console.log(resp)
         })
         .catch(error => console.log(error))
         .finally(()=> console.log('hola'))
    
+})
+     
 return(
     <div>promesa</div>
 )
