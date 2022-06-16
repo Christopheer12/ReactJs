87daaaae2a2205383dabf389be2636a75c84e1c6
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-16 00:06:18 -0300

    der

--- app_future/src/components/ItemDetail/ItemDetail.js
+++ app_future/src/components/ItemDetail/ItemDetail.js
@@ -1,12 +1,12 @@
 
-const ItemDetail = ({prod}) => {
+const ItemDetail = ({product}) => {
     
     return (
         <div class="container">
         <div class="row">
-          <div class="col-9">s</div>
+          <div class="col-9">{product.nombre}</div>
           <div class="col-4">
-          <img src={prod} alt='' className='w-50' />
+          <img src={product.foto} alt='' className='w-50' />
           </div>
           <div class="col-6">Descripcion</div>
         </div>
