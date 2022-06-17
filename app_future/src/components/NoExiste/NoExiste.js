210aecf00f4aa34325d8e2d4792b77e336cd424f
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-17 20:02:34 -0300

    sdds

--- app_future/src/components/NoExiste/NoExiste.js
+++ app_future/src/components/NoExiste/NoExiste.js
@@ -6,8 +6,7 @@ function NoExiste() {
         <div>
             <h1>Error 404</h1>
             <p>esta pagina no existe, porfavor volve atras</p>
-            <img src='https://shots.codepen.io/chrissamuels/pen/wWKzYQ-800.jpg?version=1473087446' className='w-50' />
-           
+            <img src="https://shots.codepen.io/chrissamuels/pen/wWKzYQ-800.jpg?version=1473087446"alt="" className='w-50' />
         </div>
     )
 }
