b746eba0984f00c55e493c360eff6f3b7fd50d15
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-17 19:50:09 -0300

    as

--- app_future/src/App.js
+++ app_future/src/App.js
@@ -1,7 +1,8 @@
 import NavBar from './components/Navbar/NavBar';
 import ItemListContainer from './components/containers/itemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/containers/ItemDetailContainer/ItemDetailContainer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
+import NoExiste from './components/NoExiste/NoExiste'
 
 
 function App() {
@@ -9,12 +10,13 @@ function App() {
 
   return (
     <div>
+      
       <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path='/' element={<ItemListContainer />}/>
-          <Route path='/' element={<ItemDetailContainer />}/>
-          
+          <Route path='/detalles' element={<ItemDetailContainer />}/>
+          <Route path='*' element={<NoExiste/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ app_future/src/components/NoExiste/NoExiste.js
@@ -0,0 +1,15 @@
+
+
+function NoExiste() {
+
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <p>esta pagina no existe, porfavor volve atras</p>
+            <img src='https://shots.codepen.io/chrissamuels/pen/wWKzYQ-800.jpg?version=1473087446' className='w-50' />
+           
+        </div>
+    )
+}
+
+export default NoExiste
