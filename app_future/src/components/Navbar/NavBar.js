66a8a50481a9f8eee19541798fedae3621f5ca6e
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-20 13:13:27 -0300

    nhkjlhn

--- app_future/src/components/Navbar/NavBar.js
+++ app_future/src/components/Navbar/NavBar.js
@@ -7,7 +7,8 @@ const NavBar =()=>{
   return(
     <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Future APP</a>
+      <Link to='/'class="navbar-brand">Future APP</Link>
+      
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -21,9 +22,6 @@ const NavBar =()=>{
           <Link to='/estado/Cocido' class="nav-link active" aria-current="page" >Cocido</Link></li>
           <li class="nav-item dropdown">
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
         </ul>
