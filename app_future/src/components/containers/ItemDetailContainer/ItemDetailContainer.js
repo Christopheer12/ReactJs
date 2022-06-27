bcacdb6f41175fe8a2e7d1d0dc0e27a74c2e9d9e
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-27 00:06:12 -0300

    Cart

--- app_future/src/components/containers/ItemDetailContainer/ItemDetailContainer.js
+++ app_future/src/components/containers/ItemDetailContainer/ItemDetailContainer.js
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react';
 import ItemDetail from '../../ItemDetail/ItemDetail';
 import { useParams } from 'react-router-dom';
 import { getFetch } from '../../../utils/getFetch/getFetch';
+import { Loading } from '../../../utils/Loading/Loading';
 
 
 
 const ItemDetailContainer = () => {
     const [products, setProduct] = useState({})
+    const [loading,setLoading] = useState(true)
     const { id } = useParams()
+    
     useEffect(() => {
         getFetch(id)
             .then((resp) => setProduct(resp))
             .catch(error => console.log(error))
+            .finally(()=>setLoading(false))
 
     }, [])
 
@@ -19,6 +23,7 @@ const ItemDetailContainer = () => {
     return ( 
         <div>
             <ItemDetail products={products} />
+            <Loading loading={loading}/>
         </div>
     )
 
--- app_future/src/utils/Loading/Loading.js
+++ app_future/src/utils/Loading/Loading.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Loading = ({ condition }) => {
+export const Loading = ({ loading }) => {
     return (
         <div>
-            {condition ?
+            {loading ?
                 <div>
                     <div class="spinner-border text-secondary" role="status">
                         <span class="visually-hidden">Loading...</span>
