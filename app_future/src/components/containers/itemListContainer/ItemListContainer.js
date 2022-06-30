f9bdcb7adf873b0c831575928dad120e37f02b5a
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-30 09:02:26 -0300

    ite

--- app_future/src/components/containers/itemListContainer/ItemListContainer.js
+++ app_future/src/components/containers/itemListContainer/ItemListContainer.js
@@ -1,15 +1,39 @@
 import { useEffect, useState } from 'react';
 /* import { useParams } from 'react-router-dom'; */
-import { doc, getDoc, getFirestore } from 'firebase/firestore'
+import { collection, doc, getDoc, getDocs, getFirestore } from 'firebase/firestore'
 /* import { getFetch } from '../../../utils/getFetch/getFetch'; */
 import ItemList from '../../ItemList/ItemList';
 
 const ItemListContainer = () => {
-    const [bool, setBool] = useState(true)
+    /* const [bool, setBool] = useState(true) */
     const [products, setProducts] = useState([])
-   /*  const { estadoId } = useParams() */
+    /* const { estadoId } = useParams() */
 
-    useEffect(() => {
+    /// vinculacion nativa
+
+    /*     
+        if (estadoId) {
+            getFetch()
+                .then((resp) => {
+                    setProducts(resp.filter(products => products.estado === estadoId))
+                })
+                .catch(error => console.log(error))
+    
+    
+        } else {
+            getFetch()
+                .then((resp) => {
+                    setProducts(resp)
+                })
+                .catch(error => console.log(error))
+                .finally(() => console.log())
+    
+        }
+    
+    }, [estadoId]) */
+
+    //Trayendo un solo item de firestore
+    /* useEffect(() => {
         const db = getFirestore()
         const queryItem = doc(db, 'items', '3NOKnzHPi8W5NOrxjaaZ')
         getDoc(queryItem)
@@ -18,29 +42,25 @@ const ItemListContainer = () => {
         
     }, [bool]) 
     console.log(products)
-    /* if (estadoId) {
-        getFetch()
-            .then((resp) => {
-                setProducts(resp.filter(products => products.estado === estadoId))
-            })
-            .catch(error => console.log(error))
+     */
 
 
-    } else {
-        getFetch()
-            .then((resp) => {
-                setProducts(resp)
-            })
-            .catch(error => console.log(error))
-            .finally(() => console.log())
+    useEffect(() => {
 
-    }
+        const db = getFirestore()
+        const queryCollection = collection(db, 'items')
+        getDocs(queryCollection)
+            .then(data => console.log(data))
+        console.log(products)
 
-}, [estadoId]) */
+    })
+
+
+
 
     return (
         <div>
-         {/*   { <ItemList products={products} />} */}
+            {/*   { <ItemList products={products} />} */}
         </div>
     )
 }
