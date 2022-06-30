4e33d90e300a7da4a46e7c17c5bb189e6fd32043
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-06-30 01:00:19 -0300

    iten

--- app_future/src/components/containers/itemListContainer/ItemListContainer.js
+++ app_future/src/components/containers/itemListContainer/ItemListContainer.js
@@ -1,50 +1,46 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getFetch } from '../../../utils/getFetch/getFetch';
+/* import { useParams } from 'react-router-dom'; */
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
+/* import { getFetch } from '../../../utils/getFetch/getFetch'; */
 import ItemList from '../../ItemList/ItemList';
 
-
-
 const ItemListContainer = () => {
-    const [products, setProducts] = useState([])
-    const { estadoId } = useParams()
+    const [bool, setBool] = useState(true)
+    /* const [products, setProducts] = useState([])
+    const { estadoId } = useParams() */
 
     useEffect(() => {
-        if (estadoId) {
-            getFetch()
-                .then((resp) => {
-                    setProducts(resp.filter(products => products.estado === estadoId))
-                })
-                .catch(error => console.log(error))
-            
+        const db = getFirestore()
+        const queryItem = doc(db, 'items', '3NOKnzHPi8W5NOrxjaaZ')
+        getDoc(queryItem)
+        .then(resp =>console.log(resp))
 
-        } else {
-            getFetch()
-                .then((resp) => {
-                    setProducts(resp)
-                })
-                .catch(error => console.log(error))
-                .finally(() => console.log())
-
-        }
-
-    }, [estadoId])
+    }, [bool])
+    /* if (estadoId) {
+        getFetch()
+            .then((resp) => {
+                setProducts(resp.filter(products => products.estado === estadoId))
+            })
+            .catch(error => console.log(error))
 
 
+    } else {
+        getFetch()
+            .then((resp) => {
+                setProducts(resp)
+            })
+            .catch(error => console.log(error))
+            .finally(() => console.log())
 
+    }
 
-
-
-
+}, [estadoId]) */
 
     return (
         <div>
-            <ItemList products={products} />
+           {/*  <ItemList products={products} /> */}
         </div>
     )
-
-
-
 }
 
 export default ItemListContainer
