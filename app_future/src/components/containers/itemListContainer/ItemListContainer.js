c6f3a6107ee04d1fe05b13a9533ba3c209679c65
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-07-01 20:51:01 -0300

    item

--- app_future/src/components/containers/itemListContainer/ItemListContainer.js
+++ app_future/src/components/containers/itemListContainer/ItemListContainer.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
 
 import ItemList from '../../ItemList/ItemList';
+import { useParams } from 'react-router-dom';
 
 const ItemListContainer = () => {
   
     const [products, setProducts] = useState([])
+    const {estadoId } = useParams() 
     
 
 
@@ -14,12 +16,37 @@ const ItemListContainer = () => {
 
         const db = getFirestore()
         const queryCollection = collection(db, 'items')
-        getDocs(queryCollection)
-            .then(data => setProducts(data.docs.map(item =>({id: item.id, ...item.data()})))) 
-            .catch(error => console.log(error))
+
+        if (estadoId) {
+            
+            const queryCollectionFilter = query( queryCollection, where( 'estado', '==', estadoId ) ) 
+            getDocs(queryCollectionFilter)
+            .then( data => setProducts( data.docs.map( item => ( { id: item.id, ...item.data() } )  ) ) )
+            .catch(err => console.log(err))
             .finally(() => console.log(false))
 
-    },[])
+            
+        } else {
+           
+            getDocs(queryCollection)
+            .then( data => setProducts( data.docs.map( item => ( { id: item.id, ...item.data() } )  ) ) )
+            .catch(err => console.log(err))
+            .finally(() => console.log(false))
+
+        }
+        
+    }, [estadoId])  
+
+
+
+
+       /*  const queryCollectionFilter = estado ? query(queryCollection,where('estado', '==', 'estado')) : queryCollection
+        getDocs(queryCollectionFilter)
+            .then(data => setProducts(data.docs.map(item =>({id: item.id, ...item.data()})))) 
+            .catch(error => console.log(error))
+            .finally(() => console.log(false)) */
+
+   
     console.log(products)
 
 
