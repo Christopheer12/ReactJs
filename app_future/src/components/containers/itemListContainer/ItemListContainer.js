a607d2756203c9fad2f73a9eaea35dc22a9b4637
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-07-01 13:59:09 -0300

    itemColection

--- app_future/src/components/containers/itemListContainer/ItemListContainer.js
+++ app_future/src/components/containers/itemListContainer/ItemListContainer.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { collection, doc, getDoc, getDocs, getFirestore } from 'firebase/firestore'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
 
 import ItemList from '../../ItemList/ItemList';
 
@@ -33,3 +33,18 @@ const ItemListContainer = () => {
 }
 
 export default ItemListContainer
+
+/* const ItemListContainer = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+
+        const db = getFirestore()
+        const queryCollection = collection(db, 'items')
+        const queryCollectionFilter = query(queryCollection,where('estado','==','Crudo'))
+        getDocs(queryCollectionFilter)
+            .then(data => setProducts(data.docs.map(item => ({ id: item.id, ...item.data() }))))
+            .catch(error => console.log(error))
+            .finally(() => console.log(false))
+
+    }, [])
+    console.log(products) */
