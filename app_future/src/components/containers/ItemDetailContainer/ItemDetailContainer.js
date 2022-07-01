ce56f80367c185d69d3865f7960d1b614d3c4273
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-07-01 19:26:31 -0300

    i

--- app_future/src/components/containers/ItemDetailContainer/ItemDetailContainer.js
+++ app_future/src/components/containers/ItemDetailContainer/ItemDetailContainer.js
@@ -3,35 +3,38 @@ import ItemDetail from '../../ItemDetail/ItemDetail';
 import { useParams } from 'react-router-dom';
 import { getFetch } from '../../../utils/getFetch/getFetch';
 import { Loading } from '../../../utils/Loading/Loading';
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { collection, doc, getDocs, getFirestore } from 'firebase/firestore'
 
 
 
 const ItemDetailContainer = () => {
     const [products, setProduct] = useState({})
-    const [loading,setLoading] = useState(true)
+    const [loading, setLoading] = useState(true)
     const { id } = useParams()
-    
+
     useEffect(() => {
 
-        const db = getFirestore(id)
-        const queryCollection = collection(db, 'items')
+        const db = getFirestore()
+        const queryItem = doc(db, 'items', id ) 
         getDocs(queryCollection)
             .then(data => setProducts(data.docs.map(item =>({id: item.id, ...item.data()})))) 
             .catch(error => console.log(error))
             .finally(() => console.log(false))
-       /*  getFetch(id)
-            .then((resp) => setProduct(resp))
-            .catch(error => console.log(error))
-            .finally(()=>setLoading(false))
- */
+
+
+
+        /*  getFetch(id)
+             .then((resp) => setProduct(resp))
+             .catch(error => console.log(error))
+             .finally(()=>setLoading(false))
+  */
     }, [])
 
     console.log(products)
-    return ( 
+    return (
         <div>
             <ItemDetail products={products} />
-            <Loading loading={loading}/>
+            <Loading loading={loading} />
         </div>
     )
 
