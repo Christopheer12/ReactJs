75da3956f82fa5bdb3712fa4e6fac91bdb350719
cristian guerrero <cristian1guerrero@hotmail.com>
2022-07-16 23:31:55 -0300

    master

--- app_future/src/firebase/config.js
+++ app_future/src/firebase/config.js
@@ -2,6 +2,25 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
+    apiKey: "AIzaSyDt_lxFiF6WBNbeT38xASX2vtrqISY5x54",
+    authDomain: "app-future-f0fea.firebaseapp.com",
+    projectId: "app-future-f0fea",
+    storageBucket: "app-future-f0fea.appspot.com",
+    messagingSenderId: "211016793950",
+    appId: "1:211016793950:web:014bf7d6f225cfc371d9cf"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export const getFirestoreApp = () => {
+    return app;
+}
+
+/* 
+/// principal
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
     apiKey: "AIzaSyBn4SlYjWJ0EaVFkXo0cD6VPQAbQH_j9u8",
     authDomain: "app-future-9b2dc.firebaseapp.com",
     projectId: "app-future-9b2dc",
@@ -14,4 +33,4 @@ const app = initializeApp(firebaseConfig);
 
 export const getFirestoreApp = () => {
     return app;
-}
+} */
